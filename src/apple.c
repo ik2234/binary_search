3aef99974e9539ab0bb3c405a0cdf0351460733b
ichikawa k <ichikawa.k.al@m.titech.ac.jp>
2020-07-03 14:31:24 +0900

    apple.c ok

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int A[], int n, int k, int x){
+  for(int i = 0; i < n; i++) {
+    int ceil = (A[i] + x - 1)/x; // A[i]/x の切り上げ
+    if(k < ceil) return 0;
+    else k -= ceil;
+  }
+  return 1;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +19,13 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = -1;
+  ub = 1000000000; // 10^9
+  while(ub - lb > 1){
+    int mid = (ub + lb) / 2;
+    if(mid && p(A, n, k, mid)) ub = mid; // midが0でないかつ...
+    else lb = mid;
+  }
+  printf("%d\n", ub);
   return 0;
 }
