1ee532f9ddcca8cbb6c8614c86413801d9ec058c
ichikawa k <ichikawa.k.al@m.titech.ac.jp>
2020-07-07 14:48:45 +0900

    fix init

--- src/spear.c
+++ src/spear.c
@@ -19,7 +19,7 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-  lb = -1;
+  lb = 0;
   ub = 1000000000; // 10^9
   while(ub - lb > 1){
     int mid = (ub + lb) / 2;
--- src/works.c
+++ src/works.c
@@ -24,7 +24,7 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-  lb = -1;
+  lb = 0;
   ub = 1000000000; // 10^9
   while(ub - lb > 1){
     int mid = (ub + lb) / 2;
